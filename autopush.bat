7913e85607f6ae1e4e57a2a51d3c05d28b92ccf0
Cameron Trow <tumblegamer@gamil.com>
2017-09-15 21:37:46 +0100

    server auto-save

--- autopush.bat
+++ autopush.bat
@@ -3,22 +3,27 @@ title GitHub Auto Save
 :loop
 ping localhost>nul
 git status
+echo FETCHING...
 git fetch
 echo.
 echo.
 git status
+echo ADDING...
 git add .
 echo.
 echo.
 git status
+echo COMMITING...
 git commit -m "server auto-save"
 echo.
 echo.
 git status
+echo PULLING
 git pull
 echo.
 echo.
 git status
+echo PUSHING...
 git push
 echo.
 echo.
