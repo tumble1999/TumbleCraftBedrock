5bf572017da16bbe1911940bd4ec4989c6f09533
Cameron Trow <tumblegamer@gamil.com>
2018-01-28 16:10:55 +0000

    server auto-save

--- START.bat
+++ START.bat
@@ -2,7 +2,9 @@
 echo starting...
 :loop
 cls
-set "ipGet=ipconfig | findstr /i /n 'ipv4' | findstr /i" & %ipGet% "10" & %ipGet% "48" & set ipGet=
+set "ipGet=ipconfig | findstr /i /n 'ipv4' | findstr /i"
+%ipGet% "10" & %ipGet% "48"
+set ipGet=
 echo.
 call start.cmd
 goto loop
